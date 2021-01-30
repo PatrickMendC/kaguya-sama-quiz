d514e62c881054fcfe1b86f202fb5bff9dca9fbe
PatrickMendC <patrick.navi_igb@hotmail.com>
2021-01-30 19:50:44 -0400

    add metatags na head

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import styled from 'styled-components'
 import db from '../db.json';
 
@@ -28,26 +29,37 @@ export const QuizContainer = styled.div`
 // esete componente representa a página
 export default function Home() {
   return (
-    <QuizBackground theme={db.theme} backgroundImage={db.bg}>
-      <QuizContainer>
-        <Widget>
-          <Widget.Header>
-            <h1>{db.title}</h1>
-          </Widget.Header>
-          <Widget.Content>
-            
-            <p>lorem ipsum dolor sit amet...</p>
-          </Widget.Content>
-        </Widget>
-        <Widget>
-          <Widget.Content>
-            <h1>Quiz da Kaguya</h1>
-            <p>lorem ipsum dolor sit amet...</p>
-          </Widget.Content>
-        </Widget>
-        <Footer />
-      </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/patrickmendc" />
-    </QuizBackground>
+    <>
+      <Head>
+        <title>My page title</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:title" content="Kaguya-sama QUIZ"/>
+        <meta property="og:description" content="Este é um quiz que tem o anime kaguya-sama como tema. Sendo ele desenvolvido em NextJS."/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content="https://kaguya-sama-quiz.patrickmendc.vercel.app/"/>
+        <meta property="og:image" content="kaguya-sama-quiz.png"/>
+      </Head>
+      <QuizBackground theme={db.theme} backgroundImage={db.bg}>
+        <QuizContainer>
+          <Widget>
+            <Widget.Header>
+              <h1>{db.title}</h1>
+            </Widget.Header>
+            <Widget.Content>
+              
+              <p>lorem ipsum dolor sit amet...</p>
+            </Widget.Content>
+          </Widget>
+          <Widget>
+            <Widget.Content>
+              <h1>Quiz da Kaguya</h1>
+              <p>lorem ipsum dolor sit amet...</p>
+            </Widget.Content>
+          </Widget>
+          <Footer />
+        </QuizContainer>
+        <GitHubCorner projectUrl="https://github.com/patrickmendc" />
+      </QuizBackground>
+    </>
   );
 }
